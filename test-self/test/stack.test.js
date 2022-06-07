9d6901ec278cb0462f3f359ae1bbbe95a5e92715
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-06-06 18:04:21 -0700

    Setting Operations Unit Tests

---
+++ test-self/test/stack.test.js
@@ -0,0 +1,23 @@
+describe('My Operations', () => {
+    
+it('Adding', () => {
+    expect(55 + 5).toBe(60)
+})
+
+it('Subtracting', () => {
+    expect(70 - 20).toBe(50);
+})
+
+it('Multiplying', () => {
+    expect(70 * 2).toBe(140);
+})
+
+it('Dividing', () => {
+    expect(70 / 2).toBe(35);
+})
+
+it('Modulus', () => {
+    expect(70 % 3).toBe(1);
+})
+
+})
